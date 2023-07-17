b6ead41aec38dfd7b248ded82a265707c15e1175
Ryan <ryan.vermooten@vdl.digital>
2023-07-17 11:35:00 +0200

    add route

---
+++ src/app/api/test/route.ts
@@ -0,0 +1,7 @@
+
+import { NextResponse } from 'next/server'
+
+// eslint-disable-next-line import/prefer-default-export
+export const POST = async (req: Request) => {
+  return NextResponse.json({ foo: 'bar' })
+}
